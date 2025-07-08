5493ece6905d332b887cce75120141ba68e8e3f3
Evgeni Rakintsev <euorik@yandex.com>
2025-07-08 23:34:53 +0300

    Samers fitres

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -28,7 +28,7 @@ export default {
         async fetchProfile() {
             try {
                 const tg_user = window.Telegram.WebApp.initDataUnsafe?.user
-                const response = await fetch(`http://37.1.215.204:8000/api/main/${tg_user.id}`)
+                const response = await fetch(`http://proxy-foxy.ru:8000/api/main/${tg_user.id}`)
                 const data = await response.json()
                 this.user.id = tg_user.id
                 this.user.name = tg_user.first_name
